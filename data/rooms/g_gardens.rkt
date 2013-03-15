2b40196d9bb85c81cda1264074353b58fc73513e
JP Verkamp <verkampj@indiana.edu>
2013-03-15 07:40:49 -0400

    Added gardens and graveyard.

---
+++ data/rooms/g_gardens.rkt
@@ -0,0 +1,36 @@
+(define-room 
+  [name "ballroom"]
+  [floors '(ground)]
+  [doors '(north south)]
+  [floorplan
+   '("...,,,..."
+     "..,,,...."
+     "..,,,...."
+     "...,,,~.."
+     "...,,,~.."
+     "...,,,..."
+     "....,,,.."
+     "...,,,..."
+     "...,,,...")]
+  [tiles
+   (list
+    (define-tile
+      [tile "."]
+      [name "grass"]
+      [description "overly green grass"]
+      [foreground "green"])
+    (define-tile
+      [tile ","]
+      [default-tile "."]
+      [rotated-tile "."]
+      [name "path"]
+      [foreground "gray"]
+      [description "a winding path, with small stones on either side"])
+    (define-tile
+      [tile "~"]
+      [default-tile " "]
+      [rotated-tile " "]
+      [background "gray"]
+      [name "table"]
+      [description "s tone table, it's surface scarred and pitted"]
+      [walkable #f]))])
---
+++ data/rooms/g_graveyard.rkt
@@ -0,0 +1,37 @@
+(define-room 
+  [name "graveyard"]
+  [floors '(ground)]
+  [doors '(south)]
+  [floorplan
+   '("......-.."
+     "..-..-..-"
+     "........."
+     "...-..-.."
+     "...~....."
+     "...~....."
+     "........."
+     "........."
+     ".........")]
+  [tiles
+   (list
+    (define-tile
+      [tile "."]
+      [name "grass"]
+      [description "overly green grass"]
+      [foreground "green"])
+    (define-tile
+      [tile "-"]
+      [name "tombstone"]
+      [foreground "gray"]
+      [description "a rough marble headstone, the date of death many, many years ago"])
+    (define-tile
+      [tile "~"]
+      [default-tile " "]
+      [rotated-tile " "]
+      [background "brown"]
+      [name "open grave"]
+      [description "do you really need to ask?"]
+      [walkable #t]
+      [on-walk
+       (lambda (player tile)
+         (send player die "You fall into an open grave and cannot escape.\nLooking up, you notice the name on the grave.\nIt is your own."))]))])
